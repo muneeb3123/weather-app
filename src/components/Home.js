4f93804df0e206aa03d60ff2f188e8e826c375d8
muneeb3123 <rehmanmuneeeb66@gmail.com>
2023-05-29 23:27:37 +0500

    modify

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,34 @@
 import { React, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { fetchWeatherData } from '../redux/weather/weatherslice';
 
 function Home() {
+  const navigate = useNavigate();
   const { data, isLoading } = useSelector((state) => state.weather);
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(fetchWeatherData());
   }, [dispatch]);
 
   if (isLoading) {
-    <div>Loading......</div>;
+    return <div>Loading......</div>; // Added return statement
   }
 
-  // const navigate = useNavigate();
   return data.map((city) => (
-    <div className="main" key={city.id}>
+    <div
+      tabIndex={0}
+      onClick={() => navigate('/details')}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter') {
+          navigate('/details');
+        }
+      }}
+      role="button"
+      className="main"
+      key={city.id}
+    >
       <h1 className="name">{city.name}</h1>
       <h2 className="country">{city.sys.country}</h2>
     </div>
