1f742a61e67e94871938e3815c0575ffd367e98f
muneeb3123 <rehmanmuneeeb66@gmail.com>
2023-05-30 10:26:38 +0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './components/Home/Home';
 import Details from './components/Details';
 import Navbar from './components/Navbar/Navbar';
+import './index.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={(
+            <>
+              <p className="heading">Some Cities</p>
+              <div className="grid">
+                <Home />
+              </div>
+            </>
+          )}
+        />
         <Route path="/details" element={<Details />} />
       </Routes>
     </BrowserRouter>
