bb253eada0bd30088c96f8b1b258c371ed13eb52
muneeb3123 <rehmanmuneeeb66@gmail.com>
2023-05-31 11:58:11 +0500

    Add loading and erroe conditions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { fetchWeatherData } from './redux/Home/weatherslice';
 import Home from './components/Home/Home';
 import Details from './components/Details';
 import Navbar from './components/Navbar/Navbar';
 import './index.css';
 
 function App() {
+  const { error, isLoading } = useSelector((state) => state.weather);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchWeatherData());
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <Navbar />
@@ -13,14 +22,23 @@ function App() {
           path="/"
           element={(
             <>
-              <p className="heading">Some Cities</p>
+              { isLoading && <div className="heading">Loading....</div> }
+              { error && <p className="heading">{error}</p>}
+              {!isLoading && !error && <p className="heading">Popular Cities</p>}
               <div className="grid">
                 <Home />
               </div>
             </>
-          )}
+                   )}
         />
-        <Route path="/details" element={<Details />} />
+        <Route
+          path="/details"
+          element={(
+            <div className="detail-page">
+              <Details />
+            </div>
+           )}
+        />
       </Routes>
     </BrowserRouter>
   );
