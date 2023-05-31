0582e75827b3fab318688934771b18cb37164380
muneeb3123 <rehmanmuneeeb66@gmail.com>
2023-05-31 11:58:59 +0500

    Create details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import './Details.css';
 
 function Details() {
-  const navigate = useNavigate();
   const { data } = useSelector((state) => state.Details);
 
   return (
-    <div
-      role="button"
-      tabIndex={0}
-      onClick={() => navigate('/')}
-      onKeyDown={(e) => {
-        if (e.key === 'Enter') {
-          navigate('/');
-        }
-      }}
-    >
-      {data.name}
-    </div>
+    <>
+      <div className="details-main">
+        <div className="upper">
+          <p className="country-city">
+            {data.city.name}
+            ,
+            {data.city.sys.country}
+          </p>
+          <div className="img-temp">
+            <div className="temp-condition">
+              <span className="temperature-details">
+                {data.city.main.temp}
+                &deg;C
+              </span>
+              <span className="condition-details">
+                {data.city.weather[0].main}
+              </span>
+            </div>
+            <span className="img-container">
+              <img
+                className="details-image"
+                src={data.weatherImage}
+                alt="name"
+              />
+            </span>
+          </div>
+        </div>
+        <div className="lower">
+          <h1>Temerature lists</h1>
+        </div>
+      </div>
+    </>
   );
 }
 
