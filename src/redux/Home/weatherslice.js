2f0431bf211b06ac982a9ed3cee6d2dd76ac97f6
muneeb3123 <rehmanmuneeeb66@gmail.com>
2023-05-31 11:59:42 +0500

    modify

--- src/redux/Home/weatherslice.js
+++ src/redux/Home/weatherslice.js
@@ -1,11 +1,11 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const cities = ['gujrat', 'islamabad', 'london', 'istanbul', 'mumbai', 'new york', 'muscat', 'dubai', 'yemen'];
 const key = 'fe35aab220ffb69b8fb5eef45d5615d1';
 const url = 'https://api.openweathermap.org/data/2.5/weather';
 
-export const fetchWeatherData = createAsyncThunk('weather/fetchData', async () => {
+export const fetchWeatherData = createAsyncThunk('weather/fetchData', async (city) => {
+  const cities = city ? [city] : ['Rome', 'london', 'islamabad', 'Washington, D.C', 'Ottawa', 'Tokyo', 'Edinburgh', 'Beijing', 'Stockholm'];
   const requests = cities.map((city) => {
     const params = {
       q: city,
@@ -14,15 +14,19 @@ export const fetchWeatherData = createAsyncThunk('weather/fetchData', async () =
     };
     return axios.get(url, { params });
   });
-
-  const responses = await Promise.all(requests);
-  const weatherData = responses.map((response) => response.data);
-  return weatherData;
+  try {
+    const responses = await Promise.all(requests);
+    const weatherData = responses.map((response) => response.data);
+    return weatherData;
+  } catch {
+    throw new Error('City not found. Please enter the correct city name.');
+  }
 });
 
 const initialState = {
   data: [],
   isLoading: false,
+  error: null,
 };
 
 const weatherSlice = createSlice({
@@ -31,13 +35,19 @@ const weatherSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchWeatherData.pending, (state) => ({ ...state, isLoading: true }))
+      .addCase(fetchWeatherData.pending, (state) => ({ ...state, isLoading: true, error: null }))
       .addCase(fetchWeatherData.fulfilled, (state, action) => ({
         ...state,
         isLoading: false,
         data: action.payload,
+        error: null,
       }))
-      .addCase(fetchWeatherData.rejected, (state) => ({ ...state, isLoading: false }));
+      .addCase(fetchWeatherData.rejected, (state, action) => {
+        console.log(action.error.message);
+        return {
+          ...state, data: [], isLoading: false, error: action.error.message,
+        };
+      });
   },
 });
 
