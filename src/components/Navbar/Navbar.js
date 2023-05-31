c299591e87174afe5fa8e8974dcd1d8c2a1bcad6
muneeb3123 <rehmanmuneeeb66@gmail.com>
2023-05-31 11:59:35 +0500

    add back button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { AiFillSetting } from 'react-icons/ai';
-import { IoSearch } from 'react-icons/io5';
+import { IoSearch, IoChevronBackCircleSharp } from 'react-icons/io5';
 import { FaMicrophone } from 'react-icons/fa';
-// import logo from '../../assets/weatherlogo.png';
 import './Navbar.css';
+import { fetchWeatherData } from '../../redux/Home/weatherslice';
 
 function Navbar() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const clickHandle = (value) => {
+    dispatch(fetchWeatherData(value));
+  };
+
+  const [value, setvalue] = useState('');
+  const isHomePage = location.pathname === '/';
+
+  const handleIconClick = () => {
+    navigate('/');
+  };
+
   return (
     <nav>
-      <AiFillSetting className="settingIcon" />
+      {isHomePage ? (
+        <AiFillSetting className="settingIcon" />
+      ) : (
+        <IoChevronBackCircleSharp
+          className="settingIcon"
+          onClick={handleIconClick}
+        />
+      )}
       <div className="nav-name">
         <h3 className="navbarName">Weather360</h3>
       </div>
-      <div className="search-container">
-        <input type="text" className="search-input" />
-        <span className="search-icon">
-          <IoSearch />
-        </span>
-        <FaMicrophone className="microphone" />
-      </div>
+      {isHomePage ? (
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="find cities weather"
+            className="search-input"
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                clickHandle(value);
+              }
+            }}
+            value={value}
+            onChange={(e) => setvalue(e.target.value)}
+          />
+
+          <span className="search-icon">
+            <IoSearch onClick={() => clickHandle(value)} />
+          </span>
+          <FaMicrophone className="microphone" />
+        </div>
+      ) : (
+        <IoChevronBackCircleSharp
+          className="settingIcon"
+          onClick={handleIconClick}
+        />
+      )}
     </nav>
   );
 }
